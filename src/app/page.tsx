ccc389a8861b483d36708293c83785566fa2b669
PlzRefrain <oneechansenpai123@gmail.com>
2023-10-22 03:56:24 -0500

    something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,24 +16,23 @@ export default function Investment() {
         {/* Add your graph component or image here */}
       </div>
       
-      <div className={styles.spending}>
-        <h4>Great spending habits!</h4>
-        <p> This month, you have spent $1200, $200 less than last month. Your net worth has also increased by $30000 over the past month.</p>
-       
+ <div className={styles.spendingContainer}>
+        <h4 className={styles.spendingHeader}>Great spending habits!</h4>
+        <div className={styles.spending}>
+          <p>This month, you have spent $1200, $200 less than last month. Your net worth has also increased by $30000 over the past month.</p>
+        </div>
       </div>
 
       <div className={styles.playlist}>
-        Playlist based on your spending and net worth:
+        <p>Playlist based on your spending and net worth: </p>
         {/* Add content here */}
       </div>
 
-      <div className={styles.investment}>
-        Investment, made easy with temp
-        <br />
-        What is your goal for investing today?
+      <div className={styles.investment} >
+        <h2>Investment, made easy with temp</h2>
+        <p>What is your goal for investing today?</p>
         {/* Add radio buttons or other input elements here */}
-        <br />
-        When would you like to reach the goal?
+        <p>When would you like to reach the goal?</p>
         {/* Add input elements here */}
       </div>
 
